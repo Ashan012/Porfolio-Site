b33a752624cf0e4f27e297ad5932af451971010d
ashan012 <khan89891918@outlook.com>
2025-10-10 22:02:47 +0500

    padding fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -10,7 +10,7 @@ function Header() {
         <Image
           src={assets.profile_img1}
           alt="profile image"
-          className="rounded-full w-32 mt-[70%]"
+          className="rounded-full w-32 mt-[30%]"
         />
       </div>
       <h3 className="flex items-center gap-2 text-xl md:text-2xl mb-3">
